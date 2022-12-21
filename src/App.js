4429fb03e1d506849a29b8af39c0a76774fcb6c8
Ibrahim ADLANI <ibrahim@adlani.com>
2022-12-21 18:15:57 +0100

    Instaled Commerce.js and fetched my first products.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { commerce } from './lib/commerce';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <h1>Commerce Lab</h1>
-  );
+export default function App() {
+  const [products, setProducts] = useState([]);
+
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products)
+  if (products.length) {
+    return (
+      <>
+        <h1>Commerce Lab</h1>
+        
+        <p>Commerce : { JSON.stringify(products[0]) }</p>
+      </>
+    );
+  }else{
+    return(
+      <p>loading ...</p>
+    )
+  }
+  
 }
-
-export default App;
